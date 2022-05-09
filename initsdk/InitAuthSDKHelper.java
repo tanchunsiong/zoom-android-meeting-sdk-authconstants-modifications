3f4b882d22e6153cfabdf55348fa23a5b521e938
Tan Chun Siong <dreamtcs@hotmail.com>
2022-05-09 14:20:19 +0800

    Create InitAuthSDKHelper.java

---
+++ initsdk/InitAuthSDKHelper.java
@@ -0,0 +1,80 @@
+package us.zoom.sdksample.initsdk;
+
+import android.content.Context;
+import android.os.StrictMode;
+import android.util.Log;
+
+import us.zoom.sdk.ZoomSDK;
+import us.zoom.sdk.ZoomSDKInitParams;
+import us.zoom.sdk.ZoomSDKInitializeListener;
+import us.zoom.sdk.ZoomSDKRawDataMemoryMode;
+
+/**
+ * Init and auth zoom sdk first before using SDK interfaces
+ */
+public class InitAuthSDKHelper implements  ZoomSDKInitializeListener {
+
+    private final static String TAG = "InitAuthSDKHelper";
+
+    private static InitAuthSDKHelper mInitAuthSDKHelper;
+
+    private ZoomSDK mZoomSDK;
+
+    private AuthConstants authConstants = new AuthConstants();
+
+    private InitAuthSDKCallback mInitAuthSDKCallback;
+
+    private InitAuthSDKHelper() {
+        mZoomSDK = ZoomSDK.getInstance();
+    }
+
+    public synchronized static InitAuthSDKHelper getInstance() {
+        mInitAuthSDKHelper = new InitAuthSDKHelper();
+        return mInitAuthSDKHelper;
+    }
+
+    /**
+     * init sdk method
+     */
+    public void initSDK(Context context, InitAuthSDKCallback callback) {
+        if (!mZoomSDK.isInitialized()) {
+            mInitAuthSDKCallback = callback;
+
+            ZoomSDKInitParams initParams = new ZoomSDKInitParams();
+
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+            initParams.jwtToken = authConstants.SDK_JWTTOKEN();
+            initParams.enableLog = true;
+            initParams.enableGenerateDump =true;
+            initParams.logSize = 5;
+            initParams.domain=authConstants.WEB_DOMAIN;
+            initParams.videoRawDataMemoryMode = ZoomSDKRawDataMemoryMode.ZoomSDKRawDataMemoryModeStack;
+            mZoomSDK.initialize(context, this, initParams);
+        }
+    }
+
+    /**
+     * init sdk callback
+     *
+     * @param errorCode         defined in {@link us.zoom.sdk.ZoomError}
+     * @param internalErrorCode Zoom internal error code
+     */
+    @Override
+    public void onZoomSDKInitializeResult(int errorCode, int internalErrorCode) {
+        Log.i(TAG, "onZoomSDKInitializeResult, errorCode=" + errorCode + ", internalErrorCode=" + internalErrorCode);
+
+        if (mInitAuthSDKCallback != null) {
+            mInitAuthSDKCallback.onZoomSDKInitializeResult(errorCode, internalErrorCode);
+        }
+    }
+
+    @Override
+    public void onZoomAuthIdentityExpired() {
+        Log.e(TAG,"onZoomAuthIdentityExpired in init");
+    }
+
+    public void reset(){
+        mInitAuthSDKCallback = null;
+    }
+}
